b5c1ec266049d25447964e7af5df0bc7cd7d1610
Anton Bossenbroek <anton.bossenbroek@me.com>
2017-03-25 14:10:54 +0100

    Added export of all relevant functions and classes

--- lanefinding/__init__.py
+++ lanefinding/__init__.py
@@ -1,1 +1,10 @@
 """ LaneFinding is a Python library to find lanes on movies of roads. """
+from .camera_calibration import camera_calibration
+from .thresholding import isolate_lanes
+from .Lanes import Lane
+from .process_image import perspective_correction_matrices, perspective_correct, draw_polygon
+
+__all__ = [Lane]
+__all__ += [isolate_lanes]
+__all__ += [perspective_correct, perspective_correction_matrices, draw_polygon]
+__all__ += [camera_calibration]
